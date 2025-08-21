af8a46210eabc0c249ef6bcf5c352cefeafa0b17
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-08-21 11:48:21 +0200

    clean up comments in printer

--- Sources/con/printer.swift
+++ Sources/con/printer.swift
@@ -5,17 +5,12 @@ public func printSuccess(
     outputPath: String,
     totalLines total: Int
 ) {
-    // let statusLine = "Status: " + "ok".ansi(.green)
-    let pathLine = "source: \(outputPath)".ansi(.brightBlack)
-
     let headerLine = "Concatenation " + "ok".ansi(.green, .bold)
+    let pathLine = "source: \(outputPath)".ansi(.brightBlack)
     let totalsLine = "\(total) ".ansi(.cyan) + "lines concatenated"
 
-    // print("Concatenation completed:")
     print(headerLine)
-    // print(statusLine.indent())
     print(pathLine.indent())
-    // print("\(total) lines concatenated.".indent())
     print(totalsLine.indent())
     print()
 }
